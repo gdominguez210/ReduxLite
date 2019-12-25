12a1063fed0cce4183ae75e9bf33ded8bbbaaa3a
Gary <gdominguez210@live.com>
2019-12-25 01:49:32 -0500

    Add getStore function, createStore function

--- store.js
+++ store.js
@@ -2,5 +2,12 @@ class Store {
   constructor(rootReducer) {
     this.state = {};
     this.rootReducer = rootReducer;
+    this.getStore = this.getStore.bind(this);
+  }
+
+  getStore() {
+    return Object.assign({}, this.state);
   }
 }
+
+const createStore = (...args) => new Store(...args);
